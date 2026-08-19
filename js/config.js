78b6fa93c397229c7d01ebb611ea0e0a6afa2c3e
puksaa <133646419+puksaa@users.noreply.github.com>
2026-08-19 14:04:24 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // ใส่ค่า Project URL และ Publishable/anon key จาก Supabase ก่อนเผยแพร่จริง
-export const SUPABASE_URL = 'https://qstdsnauhvcvfycbiqxy.supabase.co';
+export const SUPABASE_URL = 'https://qstdsnauhvcvfycbkqxy.supabase.co';
 export const SUPABASE_ANON_KEY = 'sb_publishable_gtk8l5AXBGpTiRaQVSpT5g__ZwEw6WW';
